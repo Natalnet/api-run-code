19b958c6a06f94a336f7c81f0f7d9e95e94bc735
root <root@elop.ect.ufrn.br>
2020-06-24 03:48:27 -0300

    adicionando api-load-balancer

--- api-load-balancer.js
+++ api-load-balancer.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
-var http = require('http');
 var https = require('https');
+var axios = require('axios');
 var privateKey  = fs.readFileSync('/etc/letsencrypt/live/exec.lop.ect.ufrn.br/privkey.pem', 'utf8');
 var certificate = fs.readFileSync('/etc/letsencrypt/live/exec.lop.ect.ufrn.br/fullchain.pem', 'utf8');
 var chain = fs.readFileSync('/etc/letsencrypt/live/exec.lop.ect.ufrn.br/chain.pem', 'utf8');
@@ -12,16 +12,35 @@ var cors = require('cors')
 
 var app = express();
 
-//middlewares globais
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-//rotas
-require('./routes')(app)
+const servers = [
+	'https://exec.lop.ect.ufrn.br:3003',
+	'https://exec.lop.ect.ufrn.br:3003',
+	'https://lop.ect.ufrn.br:3003',
+];
+let cur = 0;
+
+
+
+app.post('/apiCompiler', async (req, res) => {
+   
+   const options = {
+      headers: {
+         'Content-Type': 'application/json',	 
+      }
+   }
+
+   const response = await axios.post(`${servers[cur]}/apiCompiler`, req.body, options);
+
+   cur = (cur + 1) % servers.length;
+
+   res.send(response.data);
+})
+
 
 var httpsServer = https.createServer(credentials, app);
 
-// var httpServer = http.createServer(app);
-
-httpsServer.listen(3003, () => console.log('https on 3003'));
+httpsServer.listen(443, () => console.log('https on 443'));
