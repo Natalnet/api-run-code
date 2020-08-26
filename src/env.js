ac9eb473ef4f9364b4949a0b9d2e99a93fa4c847
Julio Melo <julio.melo@imd.ufrn.br>
2020-08-26 13:09:01 -0300

    removendo uso do process.env já que as variáveis as variáveis não são padrão do SHELL

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
 	//URL_GCC : process.env.URL_GCC || path.normalize('C:/Program Files (x86)/CodeBlocks/MinGW/bin/gcc.exe')
-	URL_GCC : process.env.URL_GPP || '/usr/bin/g++',
-	URL_GCC : process.env.URL_GCC || '/usr/bin/gcc',
+	URL_GPP : '/usr/bin/g++',
+	URL_GCC : '/usr/bin/gcc',
 	URL_PYTHON : process.env.URL_PYTHON
 }
